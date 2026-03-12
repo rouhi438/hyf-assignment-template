9e840a0a78fe007cc82369300b721a2c4ace3ee5
Abbas Rouhi <rouhi438@gmail.com>
2026-03-12 22:56:22 +0100

    Adding Task-2 CodeWars!

--- advanced-javascript/week-1/app.js
+++ advanced-javascript/week-1/app.js
@@ -10,3 +10,30 @@ for (let i = 0; i < num.length; i++) {
 const total = num
   .filter((number) => number % 2 !== 0)
   .map((sum) => (sum = sum * 2));
+
+//== 2. CodeWars! ===============================
+
+//== 8 kyu To square(root) or not to square(root)
+
+const array = [6, 4, 8, 2, 64, 11, 8, 9];
+const newArray = array.map((ar) => {
+  const root = Math.sqrt(ar);
+  if (Number.isInteger(root)) {
+    return root;
+  } else {
+    return ar * ar;
+  }
+});
+
+//== 8 kyu Removing Elements
+const names = ["Carl", "Mads", "Sara", "John", "Vlad", "Victor", "Martin"];
+
+const newName = [];
+for (let i = 0; i < names.length; i += 2) {
+  newName.push(names[i]);
+}
+console.log(newName);
+
+const newNameArray = names.filter((name, index) => {
+  return index % 2 === 0;
+});
